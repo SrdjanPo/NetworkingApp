3e99e7fb5089d9d59c7c11467a3e6fc7f4d9ce7b
SrdjanPo <ponjavusic.srdjan@gmail.com>
2019-04-11 18:14:27 +0200

    has an error, tryin' to fix

---
+++ app/src/main/java/com/example/networkingapp/activities/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.example.networkingapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.networkingapp.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
---
+++ app/src/main/java/com/example/networkingapp/activities/StartupActivity.kt
@@ -0,0 +1,22 @@
+package com.example.networkingapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.networkingapp.R
+
+class StartupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startup)
+    }
+
+    fun onLogin(v: View) {
+
+    }
+
+    fun onSignup(v: View){
+
+    }
+}
