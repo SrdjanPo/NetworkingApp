6174d183e83aad411eb9903dcf27adfd005f7717
SrdjanPo <ponjavusic.srdjan@gmail.com>
2019-04-21 23:55:54 +0200

    Database connection

--- app/src/main/java/com/example/networkingapp/activities/SignupActivity.kt
+++ app/src/main/java/com/example/networkingapp/activities/SignupActivity.kt
@@ -7,11 +7,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.networkingapp.R
+import com.example.networkingapp.User
+import com.example.networkingapp.util.DATA_USERS
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
 
+    private val firebaseDatabase = FirebaseDatabase.getInstance().reference
     private val firebaseAuth = FirebaseAuth.getInstance()
     private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
         val user = firebaseAuth.currentUser
@@ -42,6 +46,11 @@ class SignupActivity : AppCompatActivity() {
                 .addOnCompleteListener { task ->
                     if(!task.isSuccessful) {
                         Toast.makeText(this, "Signup error ${task.exception?.localizedMessage}", Toast.LENGTH_SHORT).show()
+                    } else {
+                        val email = emailET.text.toString()
+                        val userId = firebaseAuth.currentUser?.uid ?: ""
+                        val user = User(userId, "","", email, "","","")
+                        firebaseDatabase.child(DATA_USERS).child(userId).setValue(user)
                     }
                 }
         }
