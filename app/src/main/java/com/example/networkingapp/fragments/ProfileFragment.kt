f059fd5b10ad5d200a0896afc80b08a47001c7e8
SrdjanPo <ponjavusic.srdjan@gmail.com>
2019-05-30 14:28:42 +0200

    User profile - Social Media added

--- app/src/main/java/com/example/networkingapp/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/networkingapp/fragments/ProfileFragment.kt
@@ -10,10 +10,10 @@ import android.view.ViewGroup
 
 import com.example.networkingapp.R
 import com.example.networkingapp.activities.TinderCallback
-import com.example.networkingapp.profile.BasicInfoActivity
+import com.example.networkingapp.profile.*
 import com.google.firebase.database.DatabaseReference
+import kotlinx.android.synthetic.main.activity_basic_info.*
 import kotlinx.android.synthetic.main.fragment_profile.*
-
 
 class ProfileFragment : Fragment() {
 
@@ -43,12 +43,42 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Signout button
         signoutButton.setOnClickListener { callback?.onSignout() }
 
+        // Open BasicInfoActivity on click
         basicInfoEdit.setOnClickListener {
             val intentBasicInfo = Intent (getActivity(), BasicInfoActivity::class.java)
             startActivity(intentBasicInfo)
         }
+
+        // Open InterestsActivity on click
+        interestsContainer.setOnClickListener {
+            val intentInterests = Intent(getActivity(), InterestsActivity::class.java)
+            startActivity(intentInterests)
+        }
+
+
+        // Open InstagramActivity on click
+        instagramicon.setOnClickListener {
+            val intentInstagram = Intent(getActivity(), InstagramActivity::class.java)
+            startActivity(intentInstagram)
+        }
+
+
+        // Open LinkedinActivity on click
+        linkedinicon.setOnClickListener {
+            val intentLinkedin = Intent(getActivity(), LinkedinActivity::class.java)
+            startActivity(intentLinkedin)
+        }
+
+        // Open WebActivity on click
+        wwwicon.setOnClickListener {
+            val intentWeb = Intent(getActivity(), WebActivity::class.java)
+            startActivity(intentWeb)
+        }
+
+
     }
 
 
